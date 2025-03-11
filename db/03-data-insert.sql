2c43efa3c8467004aeb73524fdab22555d1d8081
Alejandro Iglesias <alejandro.adrian.iglesias@gmail.com>
2025-03-10 23:26:20 -0300

    cambio menor en error de insert

--- db/03-data-insert.sql
+++ db/03-data-insert.sql
@@ -20,4 +20,4 @@ INSERT INTO users (username, password_hash) VALUES
 -- -- Insertar datos de ejemplo en la tabla reports
 INSERT INTO reports (owner_id, result, device_id, device_name, device_type)
 VALUES (2, 'Resultados dummy', '1', 'Dispositivo de Prueba', 'switch'),
-VALUES (1, 'Resultados dummy', '1', 'Dispositivo de Prueba', 'switch');
+(1, 'Resultados dummy', '1', 'Dispositivo de Prueba', 'switch');
